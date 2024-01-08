641c6503b2e44b9b1ff011e9c5607b738ca8be9d
carmil <carmil@drata.com>
2024-01-08 11:21:13 -0800

    add cloudflare uninstall

---
+++ shell/uninstall/cloudflare.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Logging config
+LOG_NAME="cloudflare_removal.log"
+LOG_DIR="/Library/Logs"
+LOG_PATH="$LOG_DIR/$LOG_NAME"
+
+logging() {
+    # Logging function
+    #
+    # Takes in a log level and log string and logs to /Library/Logs/$script_name if a
+    # LOG_PATH constant variable is not found. Will set the log level to INFO if the
+    # first built-in $1 is passed as an empty string.
+    #
+    # Args:
+    #   $1: Log level. Examples "info", "warning", "debug", "error"
+    #   $2: Log statement in string format
+    #
+    # Examples:
+    #   logging "" "Your info log statement here ..."
+    #   logging "warning" "Your warning log statement here ..."
+    log_level=$(printf "%s" "$1" | /usr/bin/tr '[:lower:]' '[:upper:]')
+    log_statement="$2"
+    script_name="$(/usr/bin/basename "$0")"
+    prefix=$(/bin/date +"[%b %d, %Y %Z %T $log_level]:")
+
+    # see if a LOG_PATH has been set
+    if [[ -z "${LOG_PATH}" ]]; then
+        LOG_PATH="/Library/Logs/${script_name}"
+    fi
+
+    if [[ -z $log_level ]]; then
+        # If the first builtin is an empty string set it to log level INFO
+        log_level="INFO"
+    fi
+
+    if [[ -z $log_statement ]]; then
+        # The statement was piped to the log function from another command.
+        log_statement=""
+    fi
+
+    # echo the same log statement to stdout
+    /bin/echo "$prefix $log_statement"
+
+    # send log statement to log file
+    printf "%s %s\n" "$prefix" "$log_statement" >>"$LOG_PATH"
+
+}
+
+logging "--" 
+logging "`date`"
+# Check if Cloudflare is installed
+FILE=/Applications/Cloudflare\ WARP.app/Contents/Info.plist
+if [ -f "$FILE" ]; then
+    logging "Cloudflare is installed."
+    logging "Running Cloudflare ./uninstall"
+    cd /Applications/Cloudflare\ WARP.app/Contents/Resources
+    ./uninstall.sh
+    logging "Cloudflare app has been removed from Applications folder."
+    logging "--"
+else
+    logging "Cloudflare is not installed."
+    logging "--"
+fi
+
+exit 0
